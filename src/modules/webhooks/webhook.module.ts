fd99ad8912947fcf72b602e0c7fa584d065bfa34
Jmait <bolexguy@gmail.com>
2026-06-13 08:21:52 +0100

    fix: add global module to webhook

--- src/modules/webhooks/webhook.module.ts
+++ src/modules/webhooks/webhook.module.ts
@@ -1,16 +1,13 @@
 import { HttpModule } from '@nestjs/axios';
-import { Module } from '@nestjs/common';
+import { Global, Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { WebhooksController } from './controller/webhook.controller';
 import { WebhooksSnapshot } from './entity/webhook_snapshot.entity';
 import { Webhooks } from './entity/webhook.entity';
 import { WebhookService } from './service/webhook.service';
-
+@Global()
 @Module({
-  imports: [
-    HttpModule,
-    TypeOrmModule.forFeature([Webhooks, WebhooksSnapshot]),
-  ],
+  imports: [HttpModule, TypeOrmModule.forFeature([Webhooks, WebhooksSnapshot])],
   controllers: [WebhooksController],
   providers: [WebhookService],
   exports: [WebhookService],
