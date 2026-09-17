e01be91fcce4df2239e32b65f45265d6aa0fe5da
Jmait <bolexguy@gmail.com>
2026-09-17 07:46:37 +0100

    fix:add pin

--- src/infrastructure/payments/providers/vfd/vfd.client.ts
+++ src/infrastructure/payments/providers/vfd/vfd.client.ts
@@ -238,7 +238,7 @@ export class VfdClient {
         cardExpiryDate,
         acquiringInstitutionalCode: '53998359',
         sequenceNumber,
-        pin,
+        // pin,
         accountType,
         type: 'CARD',
         transactionCurrency: 'NAIRA',
